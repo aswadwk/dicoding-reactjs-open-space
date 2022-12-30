ab37cabc69f074fb166d3a0ecc157ecfc56dae6e
Hajar Aswad <hajaraswadkom@gmail.com>
2022-12-30 08:21:00 +0700

    feat: action and reducer authUser

--- src/states/authUser/action.js
+++ src/states/authUser/action.js
@@ -1,3 +1,56 @@
+import api from '../../utils/api';
+
 /**
  * @TODO: Define all the actions (creator) for the authUser state
  */
+const ActionType = {
+  SET_AUTH_USER: 'SET_AUTH_USER',
+  UNSET_AUTH_USER: 'SET_AUTH_USER',
+};
+
+function setAuthUserActionCreator(authUser) {
+  return {
+    type: ActionType.SET_AUTH_USER,
+    payload: {
+      authUser,
+    },
+  };
+}
+
+function unsetAuthUserActionCreator() {
+  return {
+    type: ActionType.UNSET_AUTH_USER,
+    payload: {
+      authUser: null,
+    },
+  };
+}
+
+function asyncSetAuthUser({ id, password }) {
+  return async (dispacth) => {
+    try {
+      const token = await api.login({ id, password });
+      api.putAccessToken(token);
+      const authUser = await api.getOwnProfile();
+
+      dispacth(setAuthUserActionCreator(authUser));
+    } catch (error) {
+      alert(error);
+    }
+  };
+}
+
+function asyncUnSetAuthUser() {
+  return (dispacth) => {
+    dispacth(unsetAuthUserActionCreator());
+    api.putAccessToken('');
+  };
+}
+
+export {
+  ActionType,
+  setAuthUserActionCreator,
+  unsetAuthUserActionCreator,
+  asyncSetAuthUser,
+  asyncUnSetAuthUser,
+};
--- src/states/authUser/reducer.js
+++ src/states/authUser/reducer.js
@@ -1,3 +1,17 @@
+import { ActionType } from './action';
+
 /**
  * @TODO: Define the reducer for the authUser state
  */
+function authUserReducer(authUser = null, action = {}) {
+  switch (ActionType.type) {
+    case ActionType.SET_AUTH_USER:
+      return action.payload.authUser;
+    case ActionType.UNSET_AUTH_USER:
+      return null;
+    default:
+      return authUser;
+  }
+}
+
+export default authUserReducer;
