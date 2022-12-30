83fe13fab0e0757388182a1b8885b250df755274
Hajar Aswad <hajaraswadkom@gmail.com>
2022-12-30 08:42:57 +0700

    feat: action and reducer users

--- src/states/users/action.js
+++ src/states/users/action.js
@@ -1,3 +1,33 @@
+import api from '../../utils/api';
+
 /**
  * @TODO: Define all the actions (creator) for the users state
  */
+const ActionType = {
+  RECEiVE_USER: 'RECEIVE_USER',
+};
+
+function receiveUserActionCretor(users) {
+  return {
+    type: ActionType.RECEiVE_USER,
+    payload: {
+      users,
+    },
+  };
+}
+
+function asyncRegisterUser({ id, name, password }) {
+  return async () => {
+    try {
+      await api.register({ id, name, password });
+    } catch (error) {
+      alert(error);
+    }
+  };
+}
+
+export {
+  ActionType,
+  receiveUserActionCretor,
+  asyncRegisterUser,
+};
